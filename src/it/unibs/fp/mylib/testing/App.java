5af36ee1617b28e12d3e305643e1b16dd99c4813
AlessandroMuscio <56693783+AlessandroMuscio@users.noreply.github.com>
2021-06-12 10:25:43 +0200

    Boh

--- src/it/unibs/fp/mylib/testing/App.java
+++ src/it/unibs/fp/mylib/testing/App.java
@@ -1,7 +1,19 @@
 package it.unibs.fp.mylib.testing;
 
-public class App {
-  public static void main(String[] args) {
+import java.util.ArrayList;
 
+import javax.xml.stream.XMLStreamException;
+
+import it.unibs.fp.mylib.XMLHandler.*;
+
+public class App {
+  public static void main(String[] args) throws XMLStreamException {
+    XMLParser parser = new XMLParser("D:/AllWorks/Projects/MyUniBSFpLib/src/it/unibs/fp/mylib/testing/PgAr_Map_5.xml");
+
+    ArrayList<City> cities = parser.parseXML(City.class);
+
+    for (City city : cities) {
+      System.out.println(city);
+    }
   }
 }
